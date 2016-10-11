23d7118746ec460b1ab49b057b962e4c419ca980
Justin Marrington <justin@marrington.net>
2016-10-11 10:47:22 +1000

    Initial commit

---
+++ source/client.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import RadialList from './components/RadialList'
+
+const mount = document.getElementById('mount')
+
+render(<RadialList />, mount)
---
+++ source/components/RadialList/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RadialList = ({
+  items
+}) => {
+
+}
+
+RadialList.propTypes = {
+  items: React.PropTypes.arrayOf(React.PropTypes.node).required
+}
+
+export default RadialList
