7a862118e18c2f89eb5bf9e767ca74aae25ef1d4
Justin Marrington <justin@marrington.net>
2016-10-12 11:51:58 +1000

    Allow radial list size customisation via 'diameter' prop

--- source/components/RadialList/index.js
+++ source/components/RadialList/index.js
@@ -6,21 +6,21 @@ import RadialListItem from '../RadialListItem'
 const BASE_STYLES = {
   display: 'block',
   position: 'relative',
-  // width: '100%',
-  // height: '100%'
-  width: '240px',
-  height: '240px',
   margin: 0,
   padding: 0
 }
 
 const RadialList = ({
   items,
+  diameter = 240, // By default renders around circle with a diameter of 240px
   offsetDegrees = -90, // Positions the first item at 12 o'clock
   listStyles = {},
   itemStyles = {}
 }) => {
-  const styles = Object.assign({}, BASE_STYLES, listStyles)
+  const styles = Object.assign({}, BASE_STYLES, {
+    width: diameter,
+    height: diameter
+  }, listStyles)
 
   return (
     <ul className={`radial-list ${cxs(styles)}`}>
@@ -40,6 +40,10 @@ const RadialList = ({
 
 RadialList.propTypes = {
   items: React.PropTypes.arrayOf(React.PropTypes.node).isRequired,
+  diameter: React.PropTypes.oneOf([
+    React.PropTypes.number,
+    React.PropTypes.string
+  ]),
   offsetDegrees: React.PropTypes.number,
   listStyles: React.PropTypes.object,
   itemStyles: React.PropTypes.object
